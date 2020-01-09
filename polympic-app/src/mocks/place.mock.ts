028cf21365cd226529816740794cc2f529ab844f
GregoryHoareau <43096860+GregoryHoareau@users.noreply.github.com>
2020-01-09 16:39:55 +0100

    Add mock for places

---
+++ polympic-app/src/mocks/place.mock.ts
@@ -0,0 +1,74 @@
+import {Place} from '../models/place';
+
+export const PLACES_MOCKED: Place[] = [
+    {
+        name: 'Tour Eiffel',
+        longitude: 2.2945,
+        latitude: 48.8584,
+    },
+    {
+        name: 'Champ de Mars',
+        longitude: 2.3049,
+        latitude: 48.8562,
+    },
+    {
+        name: 'Grand Palais',
+        longitude: 2.31376,
+        latitude: 48.8661,
+    },
+    {
+        name: 'Esplanade des Invalides',
+        longitude: 2.31291,
+        latitude: 48.8603,
+    },
+    {
+        name: 'Château de Versailles',
+        longitude: 2.12137,
+        latitude: 48.8045,
+    },
+    {
+        name: 'Stade de France',
+        longitude: 2.36257,
+        latitude: 48.925,
+    },
+    {
+        name: 'Champ-Élysées',
+        longitude: 2.30533,
+        latitude: 48.8708,
+    },
+    {
+        name: 'Parc des expositions de la porte de Versailles',
+        longitude: 2.28841,
+        latitude: 48.8319,
+    },
+    {
+        name: 'Parc des Princes',
+        longitude: 2.25155,
+        latitude: 48.8419,
+    },
+    {
+        name: 'Place de la Concorde',
+        longitude: 1.96207,
+        latitude: 48.7768,
+    },
+    {
+        name: 'Stade Jean-Bouin',
+        longitude: 2.25492,
+        latitude: 48.8437,
+    },
+    {
+        name: 'Stade Roland-Garros',
+        longitude: 2.25083,
+        latitude: 48.8457,
+    },
+    {
+        name: 'Palais omnisports de Paris-Bercy',
+        longitude: 2.37797,
+        latitude: 48.8391,
+    },
+    {
+        name: 'Zénith de Paris',
+        longitude: 2.393333333333333,
+        latitude: 48.8941667,
+    },
+];
