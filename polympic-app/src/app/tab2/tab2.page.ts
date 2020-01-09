d8d724908f5929a6fd06028f168e666cbf8a0e36
Pierre-Antoine Boulat <pierre-antoine.boulat@etu.unice.fr>
2020-01-09 11:22:48 +0100

    init leaflet map

--- polympic-app/src/app/tab2/tab2.page.ts
+++ polympic-app/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  
+  map: Map;
+  
   constructor() {}
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([48.9244592, 2.3601645], 15);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    marker([48.9244592, 2.3601645]).addTo(this.map)
+      .bindPopup('Stade de France <br>Football')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 
 }
