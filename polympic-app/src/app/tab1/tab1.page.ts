7535b5bad5396c8d9c359b3c89875b6942f1309f
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2020-01-10 13:24:36 +0100

    #13 can filter the list of events by name or by sport preference...

--- polympic-app/src/app/tab1/tab1.page.ts
+++ polympic-app/src/app/tab1/tab1.page.ts
@@ -14,13 +14,25 @@ import { SPORTS_MOCKED } from './../../mocks/sport.mock'
 export class Tab1Page {
 
   events: Event[];
+  eventsService;
   selected_event;
   
   constructor(service: EventsService, private navCtrl: NavController) {
     this.events = service.events;
+    this.eventsService = service.events;
   }
 
   getSportIcon(sport) {
     return SPORTS_MOCKED[sport];
+  }
+
+  filterEvents(e) {
+    this.events = this.eventsService;
+    const val = e.target.value;
+    if(val && val.trim() != '') {
+      this.events = this.events.filter( event => {
+        return (event.name.toLowerCase().indexOf(val.toLowerCase()) > -1) || (event.type.toLowerCase().indexOf(val.toLowerCase()) > -1); 
+      } )
+    }
   }
 }
