94213c9c07105cbc6f84b487148e620f9d66f48b
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2020-01-10 15:24:56 +0100

    #15 Correction of a bug when click on back button

--- polympic-app/src/app/components/components.module.ts
+++ polympic-app/src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 
 import { HeaderComponent } from './../header/header.component';
+import { HeaderTabsComponent } from './../header-tabs/header-tabs.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -14,7 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
         FormsModule,
         FontAwesomeModule
     ],
-    declarations: [HeaderComponent],
-    exports: [HeaderComponent]
+    declarations: [HeaderComponent, HeaderTabsComponent],
+    exports: [HeaderComponent, HeaderTabsComponent]
 })
 export class ComponentsModule {}
---
+++ polympic-app/src/app/header-tabs/header-tabs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-tabs',
+  templateUrl: './header-tabs.component.html',
+  styleUrls: ['./header-tabs.component.scss'],
+})
+export class HeaderTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
