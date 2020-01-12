0c261b2121ad52b404dd5e938a16e8a4eb6c1b8a
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2020-01-12 12:41:18 +0100

    #11 add more mocks of events. #13 The sports filters are well displayed now

--- polympic-app/src/mocks/sportFilter.mock.ts
+++ polympic-app/src/mocks/sportFilter.mock.ts
@@ -8,5 +8,15 @@ export const SPORTS_FILTERS_MOCKED = [
         name: 'Athlétisme',
         image: './assets/athletic.jpg',
         clicked: false
+    },
+    {
+        name: 'Football',
+        image: './assets/football.jpg',
+        clicked: false
+    },
+    {
+        name: 'Basketball',
+        image: './assets/basketball.jpg',
+        clicked: false
     }
 ]
--- polympic-app/src/mocks/sportIcons.mock.ts
+++ polympic-app/src/mocks/sportIcons.mock.ts
@@ -1,9 +1,10 @@
-import { faSwimmer, faFutbol, faRunning, faBicycle } from '@fortawesome/free-solid-svg-icons';
+import { faSwimmer, faFutbol, faRunning, faBicycle, faBasketballBall } from '@fortawesome/free-solid-svg-icons';
 
 
 export const SPORTS_ICONS_MOCKED = {
     Natation: faSwimmer,
     Athlétisme: faRunning,
     Football: faFutbol,
-    Vélo: faBicycle
+    Vélo: faBicycle,
+    Basketball: faBasketballBall
 }
